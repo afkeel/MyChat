1d8e33356b585dd4c576b74490871887513ffaff
ArtemF <blademasterii@rambler.ru>
2019-05-05 00:28:47 +0500

    Test Firebase

--- app/src/main/java/com/example/mychat/MainActivity.java
+++ app/src/main/java/com/example/mychat/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.mychat;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
@@ -24,6 +30,24 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 myRef.setValue("Hello, World!");
+            }
+        });
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(getClass().getName(), "Value is: " + value);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Failed to read value
+
             }
         });
     }
