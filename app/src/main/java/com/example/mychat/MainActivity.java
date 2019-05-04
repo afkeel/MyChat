9724c148fe2125e35e9c59f2d50a2bfb03c8a17f
ArtemF <blademasterii@rambler.ru>
2019-05-04 22:16:51 +0500

    Test Firebase

--- app/src/main/java/com/example/mychat/MainActivity.java
+++ app/src/main/java/com/example/mychat/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.mychat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                myRef.setValue("Hello, World!");
+            }
+        });
     }
 }
